53335c3e907d911d610697d01b10634ba1192334
Bruce Muthomi Guantai <muthomiguantai@gmail.com>
2020-01-30 12:42:47 +0300

    es5 basics and operations

---
+++ index.js
@@ -0,0 +1,32 @@
+//name cannot be a reserved keyword
+//should be meaningful
+//cannot start with a number
+//cannot contain space or hyphen
+//case sensitive
+
+//variables
+let firstName = "Bruce";
+let lastName = "Muthomi";
+
+//const
+const interestRate = 0.3;
+interestRate = 1;
+
+//if you want to reassign use let and
+//if you dont want to re-assign use const
+
+//primitive types
+//string,number,boolean,undefined,null
+let name = "Guantai"; //dtring
+let age = 25; //number
+let IsApproved = true; //boolean
+let firstName = undefined; //undefined
+let lastName = null; //null
+
+//Reference Types
+//object,array,Function
+let person = {
+  name: "Muthomi",
+  age: 30
+};
+console.log(person);
